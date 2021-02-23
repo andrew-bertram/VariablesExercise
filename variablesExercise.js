86599f62c83b57301c83fa374ff01d6e35864d43
Andrew Bertram <andrew.bertram23@gmail.com>
2021-02-23 11:41:07 -0500

    Exercise completed including bonus.

---
+++ variablesExercise.js
@@ -0,0 +1,43 @@
+// 1
+// Const
+
+// 2
+let num = 20;
+
+// 3
+num = 15
+
+// 4
+num = num + 7;
+// console.log(num);
+
+// 5
+num --;
+// console.log(num);
+
+// 6
+num = "This is not a number";
+
+// 7
+let blank;
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+// console.log(blank);
+
+// BONUS
+
+// 1
+const [quarter,dime,nickel,penny] = [0.25,0.10,0.05,0.01];
+
+// 2
+console.log(quarter,dime,nickel,penny);
+
+// 3
+let letters = "qwertyuiop";
+
+// 4
+console.log(letters[9].length);
